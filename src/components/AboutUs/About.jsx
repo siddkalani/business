9c4a369cd0c22935b5c5b378cfce26676805b90d
siddkalani <siddharthkalani6@gmail.com>
2024-09-16 01:43:32 +0530

    s

--- src/components/AboutUs/About.jsx
+++ src/components/AboutUs/About.jsx
@@ -4,9 +4,6 @@ const About = () => {
     return (
         <>
             <div>
-                <noscript>
-                    {`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59FFWG7"        height="0" width="0" style="display:none;visibility:hidden"></iframe>`}
-                </noscript>
                 <header
                     className="site-header"
                     id="masthead"
